459cbd1918cf21dd91933965e66f6accd12a79b6
carlos.melgarejo.solorzano@hotmail.com <carlos.melgarejo.solorzano@hotmail.com>
2025-07-08 18:18:33 -0500

    Add: Ok

--- src/Cart/Cart.WebAPI/Program.cs
+++ src/Cart/Cart.WebAPI/Program.cs
@@ -8,7 +8,7 @@ using Serilog.Events;
 using Serilog.Formatting.Compact;
 using Shared.Infrastructure.Extensions;
 
-// Bootstrap logger!!!!
+// Bootstrap logger!!!!1
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
     .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}")
